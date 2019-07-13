3ae700ec6d4a2d826c091e582186a63ef1511941
zhoushaodong <1138894663@qq.com>
2019-07-13 19:12:53 +0800

    helloworld与变量

--- 1-helloworld与变量/helloworld.py
+++ 1-helloworld与变量/helloworld.py
@@ -1,6 +1,13 @@
 # 第一个程序 hello world
 print("hello world")
 
+# 变量名命名的规范
+# 变量名只可以包含 字母、数字、下划线。并且只有 字母、下划线可以开头
+# 例如 name1 _name是正确的命名, 2name则是错误的命名
+# 变量名不能包含空格 student name是错误 我们可以用下划线分隔 student_name
+# 变量名不能使用python的一些关键字 就像 print 函数  这是python的保留字
+
+
 # 我们用一个变量来存 hello world
 message = "hello world!!"
 print(message)
@@ -9,8 +16,3 @@ print(message)
 message = "hello python!!"
 print(message)
 
-# 变量名命名的规范
-# 变量名只可以包含 字母、数字、下划线。并且只有 字母、下划线可以开头
-# 例如 name1 _name是正确的命名, 2name则是错误的命名
-# 变量名不能包含空格 student name是错误 我们可以用下划线分隔 student_name
-# 变量名不能使用python的一些关键字 就像 print 函数  这是python的保留字
