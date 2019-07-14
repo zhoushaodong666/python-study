2f3218314b1a836d132328ec82bb58460e18979a
zhoushaodong <1138894663@qq.com>
2019-07-14 17:45:16 +0800

    布尔类型

--- 2-简单数据类型/bool_test.py
+++ 2-简单数据类型/bool_test.py
@@ -1,11 +1,11 @@
 # bool 布尔值 真/甲
 # 真用"True"表示
-bool_true = True  #注意 不能用 true表示 编译器会提示语法错误
+bool_true = True  #注意 不能用 true表示 不然编译器会报错
 print(type(bool_true)) # <class 'bool'>
 print(bool_true) # True
 
 # 假用"Flase"表示
 
-bool_false = False #注意 不能用 false表示 编译器会提示语法错误
+bool_false = False #注意 不能用 false表示 不然编译器会报错
 print(type(bool_false)) # <class 'bool'>
 print(bool_false) # False
