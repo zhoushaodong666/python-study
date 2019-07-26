9b0a78beb05ab8be59e5c5ed0a8086e040d47472
zhoushaodong666 <1138894663@qq.com>
2019-07-26 14:04:09 +0800

    条件语句

---
+++ 5-条件语句/condition.py
@@ -0,0 +1,22 @@
+#  1.简单的if语句
+# age = 20
+# if age >= 18:
+#     print("已成年")
+
+# 2.if-else语句
+# age = 17
+# if age >= 18:
+#     print("已成年")
+# else:
+#     print("未成年")
+
+# 3.if-elif-else语句
+
+age = 10
+if age < 8:
+    print("8岁以下免票")
+elif age < 18:
+    print("18岁以下半价票")
+else:
+    print("全价票")
+
