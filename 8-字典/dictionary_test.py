e8261c652d2fca1ee12d1767c9a365033cbb5fb2
zhoushaodong <1138894663@qq.com>
2019-08-16 23:10:28 +0800

    字典

--- 8-字典/dictionary_test.py
+++ 8-字典/dictionary_test.py
@@ -6,3 +6,19 @@ dict1 = {'a': 1, 'b': 2, 'b': '3'}
 print(dict1)  # {'a': 1, 'b': '3'}
 print(type(dict1))  # <class 'dict'>
 
+# 字典的访问
+dict2={"name": "tom", "age": 12}
+print(dict2)  # 'name': 'tom', 'age': 12}
+print(dict2["name"])  # tom
+print(dict2["age"])  # 12
+
+# 访问字典中不存在的键 会抛出错误
+
+#print(dict2["aa"]) KeyError: 'aa'
+
+# 字典中键值的修改
+dict3 = {"name": "david", "age": 18}
+print(dict3)  # {'name': 'david', 'age': 18}
+dict3["name"] = "atom"  # 修改dict3中键名为name的值为atom
+dict3["age"] = 20  # # 修改dict3中键名为age的值为20
+print(dict3)  # {'name': 'atom', 'age': 20}
