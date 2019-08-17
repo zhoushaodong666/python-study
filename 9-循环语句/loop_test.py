666238bf6dad755c002ab2c17737781015e1e123
zhoushaodong <1138894663@qq.com>
2019-08-17 19:40:16 +0800

    循环语句

---
+++ 9-循环语句/loop_test.py
@@ -0,0 +1,16 @@
+# 循环语句
+# while循环
+total = 0 # 定义一个变量 初始值为0
+while total < 10:  # 循环条件是total<10 执行循环语句块内容
+    total += 1  # 每次循环total增加1
+    print("total:",total)  # 每次循环输出total的值
+
+# while-else语句 类似于if-else语句
+loop1 = 0
+while loop1<5:
+    print(loop1, "loop1 小于5:")  # 每次循环输出total的值
+    loop1 += 1  # 每次循环loop1增加1
+
+else:
+    print(loop1, "不小于5")
+# for循环
