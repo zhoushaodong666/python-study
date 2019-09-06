107274cc814fc22ac055bf409c4e2926c7c567ce
zhoushaodong <1138894663@qq.com>
2019-09-06 22:40:56 +0800

    函数

--- 12-函数/def_test.py
+++ 12-函数/def_test.py
@@ -4,12 +4,29 @@
 # def 函数名（参数列表）:
 #     函数体
 
+# 定义一个函数 函数允许有return返回值 如果没有默认返回None
+def funcname(parm):
+    pass
+
+
+# 函数调用
+
+# 如果函数调用在函数定义之前 程序会报错：NameError: name 'printHelloWorld' is not defined
+# python是解释性的语言 会按照先后循环进行编译
+# 函数调用一定要在函数定义之后
+# printHelloWorld()
+
 def printHelloWorld():
     print("HelloWorld")
 
-# 函数的调用
+
 printHelloWorld()  # HelloWorld
 
 
+# 定义一个含有返回值的函数
+def add(x, y):
+    return x + y
 
 
+result = add(10,20)
+print(result) # 30
