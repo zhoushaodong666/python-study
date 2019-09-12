ffeafc866d2addd9809222692f4ff697a75acc9c
zhoushaodong <1138894663@qq.com>
2019-09-12 23:10:57 +0800

    面向对象

---
+++ 14-面向对象/class_test1.py
@@ -0,0 +1,28 @@
+# 类的定义
+# 类是具有相同特征的一类事物,描述具有相同的属性和方法的对象的集合
+
+# 格式：
+"""
+class ClassName():
+    类体
+"""
+
+# 写一个简单的类
+class Student():
+    # 类的属性
+    age = 0
+    name = ''
+
+    # 类的方法
+    def printHello(self):
+        print("hello")
+
+s = Student()
+s.printHello()
+
+
+
+
+
+
+
