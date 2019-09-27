3f6c20a4e649014b0455005fc4539ccf7b29e408
zhoushaodong <1138894663@qq.com>
2019-09-27 23:05:48 +0800

    面向对象

--- 14-面向对象/class_test5.py
+++ 14-面向对象/class_test5.py
@@ -14,7 +14,15 @@ class Student(Person):
     def __init__(self,id,school,name,age):
         self.id = id
         self.school = school
-        Person.__init__(self,name,age)
+        # 第1种调用父类构造函数的方法：父类名.__init__()(不推荐)
+
+        # 此时要传入self实例对象 是因为平常我们直接 【对象名.方法名()】是传入了self对象的
+        # 如果使用【类名.方法名()】是没有传入self对象的，所以需要显示的传入对象名self
+        #Person.__init__(self,name,age)
+
+        # 第2种调用父类构造函数的方法: super(子类名,对象名).__init__(参数列表) (推荐)
+        # python提供的super就是用来调用父类方法的
+        super(Student,self).__init__(name,age)
 
 s1 = Student("0101","清华大学","小白",22)
 print(s1.__dict__)
