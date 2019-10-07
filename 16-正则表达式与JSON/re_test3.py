d6259f8607525d320e1286bf87ce3b8d43f72490
zhoushaodong <1138894663@qq.com>
2019-10-07 16:50:34 +0800

    正则表达式

--- 16-正则表达式与JSON/re_test3.py
+++ 16-正则表达式与JSON/re_test3.py
@@ -1,14 +1,14 @@
 import re
 
-# 字符集 中括号"[]"
-# [amd] 匹配字符'a'或'm'或'd'
+# 运算符 中括号"[]"
+# [amd] 匹配单个字符'a'或'm'或'd'
 
 # 匹配中间字符是'b'、'c'、'd',第一个字符是'a',第三个字符是'd'的全部字符串
 str1 = "abd,abc,add,aef,aed,acc,aee,aff,aed,acd"
 list1 = re.findall("a[bcd]d",str1)
 print(list1)  # ['abd', 'add', 'acd']
 
-# bcd是连续的 我们可以使用"-",b-d表示b到d的全部字母，即bcd
+# bcd是连续的 我们可以使连字号"-",b-d表示b到d的全部字母，即bcd
 list2 = re.findall("a[b-d]d",str1)
 print(list2)  # ['abd', 'add', 'acd']
 
