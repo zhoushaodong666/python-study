e2c9e2073e291b1aa3572b7f5a287c463828636f
zhoushaodong <1138894663@qq.com>
2019-10-13 23:34:46 +0800

    正则表达式

--- 16-正则表达式与JSON/re_test6.py
+++ 16-正则表达式与JSON/re_test6.py
@@ -1,3 +1,13 @@
 import re
 
 # 组
+# 使用"()"将指定匹配的序列分为一组
+str = "PythonPythonPythonPythonPythonJavaJavaJavaJava"
+
+# 匹配是否有5个连续Python的字符串
+list = re.findall("(Python){5}",str)
+print(list)  # ['Python']
+
+# 匹配是否有6个连续Python的字符串
+list2 = re.findall("(Python){6}",str)
+print(list2)  # []
