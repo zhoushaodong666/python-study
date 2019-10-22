dc45525cc02d21c345f7b516cbae532f3240eeba
zhoushaodong <1138894663@qq.com>
2019-10-22 22:25:55 +0800

    正则表达式

--- 16-正则表达式与JSON/re_test10.py
+++ 16-正则表达式与JSON/re_test10.py
@@ -9,9 +9,15 @@ mat = re.search("Life(.*)ocean and(.*)",str)
 print(mat.group())  # Life is like the ocean and is beautiful
 print(mat.group(0))  # Life is like the ocean and is beautiful
 
-# group(1)为内部的第1个分组匹配的内容
+# group(1)为内部的第1个索引分组匹配的内容
 print(mat.group(1))  #  is like the
 
-# group(2)为内部的第2个分组匹配的内容
+# group(2)为内部的第2个索引分组匹配的内容
 print(mat.group(2))  #  is beautiful
+
+# 一次输出第0 1 2个索引分组匹配的内容
+print(mat.group(0,1,2))  # 'Life is like the ocean and is beautiful', ' is like the ', ' is beautiful')
+
+# 一次输出自定义分组匹配的内容
+print(mat.groups())  # (' is like the ', ' is beautiful')
 
