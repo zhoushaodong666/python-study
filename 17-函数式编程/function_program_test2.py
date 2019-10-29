dd253ee7826c333ed5cc93da1dc6807643f703f4
zhoushaodong <1138894663@qq.com>
2019-10-29 23:02:07 +0800

    三元表达式

---
+++ 17-函数式编程/function_program_test2.py
@@ -0,0 +1,13 @@
+# 三元表达式
+
+# x1大于y1,res1等于x1，否则等于y1
+x1 = 1
+y1 = 2
+res1 = x1 if x1 > y1 else y1
+print(res1)  # 2
+
+# x2大于y2,res2等于x2，否则等于y2
+x2 = 3
+y2 = 2
+res2 = x2 if x2 > y2 else y2
+print(res2)  # 3
