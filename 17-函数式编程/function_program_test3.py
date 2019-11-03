ffba8e0fcda7fefdf4bbb292cabb9fced31c4ba0
zhoushaodong <1138894663@qq.com>
2019-11-03 23:23:08 +0800

    map与lambda表达式

--- 17-函数式编程/function_program_test3.py
+++ 17-函数式编程/function_program_test3.py
@@ -6,9 +6,14 @@ def double_num(x):
     return 2*x
 
 list1 = [1,2,3,4,5]
+list2 = [2,4,6,8,10]
 res = map(double_num,list1)
 print(list(res))  # [2, 4, 6, 8, 10]
 
 # map与lambda表达式
 res2 = map(lambda x:2*x,list1)
 print(list(res2))  # [2, 4, 6, 8, 10]
+
+# map第二个参数是可变参数 可以传入多个参数
+res3 = map(lambda x,y:x+2*y,list1,list2)
+print(list(res3))  # [5, 10, 15, 20, 25]
